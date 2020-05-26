46af0a4b69657bba9b31415ad8f20d85cac41933
drenner <renner@cscs.ch>
2020-05-26 23:56:22 +0200

    Cluster update.

--- examples/transmission_problem_verification.cpp
+++ examples/transmission_problem_verification.cpp
@@ -1,28 +1,36 @@
 #include <complex>
 #include <iostream>
+#include <fstream>
 #include "parametrized_circular_arc.hpp"
 #include "solvers.hpp"
 #include "generate_solution.hpp"
+#include "continuous_space.hpp"
+#include "discontinuous_space.hpp"
+#include "mass_matrix.hpp"
 
 typedef std::complex<double> complex_t;
 complex_t ii = complex_t(0,1.);
-int main() {
+int main(int argc, char** argv) {
 
     // define radius of circle
-    double eps = 0.25;
-    int l = 10;
+    double eps = atof(argv[1]);
+    std::cout << argv[7] << std::endl;
+    int l = atoi(argv[2]);
     double a_n[2*l+1];
-    double c_i = 23.;
-    double c_o = 1.;
-    double k = 1.;//2.75679178324354;
+    double c_i = atof(argv[3]);
+    double c_o = atof(argv[4]);
+    double k = atof(argv[5]);//2.75679178324354;
     ParametrizedCircularArc curve(Eigen::Vector2d(0,0),eps,0,2*M_PI);
-    unsigned order = 11;
+    unsigned order = atoi(argv[6]);
     for( int i = 0; i<2*l+1; i++) {
         a_n[i] = 1./((k*k*(c_o-c_i))*sqrt((2*l+1)*M_PI*eps*eps*(jn(i-l,k)*jn(i-l,k)-jn(i-l-1,k)*jn(i-l+1,k))));
         std::cout << a_n[i] << std::endl;
     }
+    std::ofstream filename;
+    filename.open(argv[7], std::ofstream::out | std::ofstream::trunc);
+    filename.close();
     unsigned n_runs = 7;
-    double numpanels[n_runs];
+    int numpanels[n_runs];
     numpanels[0] = 50;
     for (int i=1; i<n_runs; i++){
         numpanels[i] = 2*numpanels[i-1];
@@ -39,11 +47,23 @@ int main() {
     auto u_t_neu = [&] (double x1, double x2) {
         return sol::u_t_N(x1, x2, l, eps, a_n, k, c_i);
     };
+    DiscontinuousSpace<0> discont_space;
+    ContinuousSpace<1> cont_space;
     // Loop over number of panels
     for (unsigned i = 0; i <= n_runs; i++) {
         ParametrizedMesh mesh(curve.split(numpanels[i]));
-        Eigen::VectorXcd Tn_dfk = tsp::direct_second_kind::solve(
+        Eigen::VectorXcd sol = tsp::direct_second_kind::solve(
                 mesh, u_i_dir, u_i_neu, u_t_dir, u_t_neu, order, k, c_o, c_i);
+        Eigen::MatrixXcd M_cont = mass_matrix::GalerkinMatrix(mesh,cont_space,cont_space,order);
+        Eigen::MatrixXcd M_discont = mass_matrix::GalerkinMatrix(mesh,discont_space,discont_space,order);
+        Eigen::MatrixXcd M(2*numpanels[i],2*numpanels[i]);
+        M.block(0,0,numpanels[i],numpanels[i]) = M_cont;
+        M.block(numpanels[i],numpanels[i],numpanels[i],numpanels[i]) = M_discont;
+
+        filename.open(argv[7], std::ios_base::app);
+        filename << mesh.getPanels()[0]->length() << " " << sqrt(abs(sol.dot(M*sol))) << std::endl;
+        filename.close();
+
     }
     return 0;
 }
