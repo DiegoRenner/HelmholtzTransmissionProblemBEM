9bc2180c5694538e3a5556b6fc2fc86db4720e6c
drenner <renner@cscs.ch>
2020-06-24 11:41:13 +0200

    improved autoinstall

--- cmake/functions/Getcomplex_bessel.cmake
+++ cmake/functions/Getcomplex_bessel.cmake
@@ -4,21 +4,17 @@
 #				COMMAND cd build
 #				COMMAND make install
 #				COMMAND echo test)
-execute_process(COMMAND echo test)
 execute_process(COMMAND git clone git@github.com:DiegoRenner/complex_bessel
 				WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}")
-			execute_process(COMMAND bash build.sh "${PROJECT_SOURCE_DIR}"
+execute_process(COMMAND mkdir complex_bessel_lib
+				WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}")
+execute_process(COMMAND bash build.sh "${PROJECT_SOURCE_DIR}/complex_bessel_lib"
 				WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}/complex_bessel")
 execute_process(COMMAND make install
 				WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}/complex_bessel/build")
 execute_process(COMMAND ls complex_bessel_lib
 				WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}")
-execute_process(COMMAND cp -r complex_bessel_lib/include/. include
-				WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}")
-execute_process(COMMAND cp -r complex_bessel_lib/lib .
-				WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}")
-link_directories(${PROJECT_SOURCE_DIR}/lib)
-execute_process(COMMAND rm -rf complex_bessel
-			WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}")
-execute_process(COMMAND rm -rf complex_bessel_lib
-				WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}")
+include_directories(${PROJECT_SOURCE_DIR}/complex_bessel_lib/include)
+link_directories(${PROJECT_SOURCE_DIR}/complex_bessel_lib/lib)
+#execute_process(COMMAND rm -rf complex_bessel
+#			WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}")
