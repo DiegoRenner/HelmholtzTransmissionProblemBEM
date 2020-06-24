6bbac9cc77fa27bcb71550fc52e21736078a050a
drenner <renner@cscs.ch>
2020-06-24 12:28:18 +0200

    added check for complex_bessel library to autoinstall

--- cmake/functions/Getcomplex_bessel.cmake
+++ cmake/functions/Getcomplex_bessel.cmake
@@ -1,20 +1,39 @@
-#execute_process(COMMAND git clone https://www.github.com/DiegoRenner/complex_bessel
-#				COMMAND cd complex_bessel
-#				COMMAND bash build.sh
-#				COMMAND cd build
-#				COMMAND make install
-#				COMMAND echo test)
-execute_process(COMMAND git clone git@github.com:DiegoRenner/complex_bessel
-				WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}")
-execute_process(COMMAND mkdir complex_bessel_lib
-				WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}")
-execute_process(COMMAND bash build.sh "${PROJECT_SOURCE_DIR}/complex_bessel_lib"
-				WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}/complex_bessel")
-execute_process(COMMAND make install
-				WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}/complex_bessel/build")
-execute_process(COMMAND ls complex_bessel_lib
-				WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}")
-include_directories(${PROJECT_SOURCE_DIR}/complex_bessel_lib/include)
-link_directories(${PROJECT_SOURCE_DIR}/complex_bessel_lib/lib)
-execute_process(COMMAND rm -rf complex_bessel
-			WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}")
+if(NOT EXISTS /usr/lib/libcomplex_bessel.so)
+	if(NOT EXISTS "${PROJECT_SOURCE_DIR}/complex_bessel_lib")
+		message(STATUS "No complex_bessel library found, installing locally.")
+		execute_process(COMMAND git clone git@github.com:DiegoRenner/complex_bessel
+						WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}")
+		execute_process(COMMAND mkdir complex_bessel_lib
+						WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}")
+		execute_process(COMMAND bash build.sh "${PROJECT_SOURCE_DIR}/complex_bessel_lib"
+						WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}/complex_bessel")
+		execute_process(COMMAND make install
+						WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}/complex_bessel/build")
+		include_directories(${PROJECT_SOURCE_DIR}/complex_bessel_lib/include)
+		link_directories(${PROJECT_SOURCE_DIR}/complex_bessel_lib/lib)
+		execute_process(COMMAND rm -rf complex_bessel
+					WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}")
+	else()
+		message(STATUS "Found complex_bessel library installed locally.")
+	endif()
+else()
+	message(STATUS "Found complex_bessel library installed in /usr/lib directory.") 
+	if(NOT EXISTS "${PROJECT_SOURCE_DIR}/include/complex_bessel.h")
+		message(STATUS "Headers are missing, grabbing them.")
+		execute_process(COMMAND git clone git@github.com:DiegoRenner/complex_bessel
+						WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}")
+		execute_process(COMMAND mkdir complex_bessel_lib
+						WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}")
+		execute_process(COMMAND bash build.sh "${PROJECT_SOURCE_DIR}/complex_bessel_lib"
+						WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}/complex_bessel")
+		execute_process(COMMAND make install
+						WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}/complex_bessel/build")
+		execute_process(COMMAND cp -a complex_bessel_lib/include/. include
+						WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}")
+		execute_process(COMMAND rm -rf complex_bessel
+					WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}")
+		execute_process(COMMAND rm -rf complex_bessel_lib
+					WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}")
+	endif()
+endif()
+
