44a7c64972497d79c564eaa21eef2d015877d7aa
drenner <renner@cscs.ch>
2021-02-11 22:47:48 +0100

    pulling external projects via https for easier access on cluster

--- cmake/functions/Getarpackpp.cmake
+++ cmake/functions/Getarpackpp.cmake
@@ -1,6 +1,6 @@
 if(NOT EXISTS "${PROJECT_SOURCE_DIR}/arpackpp_lib")
 		message(STATUS "No arpackpp library found, installing from github.")
-		execute_process(COMMAND git clone git@github.com:m-reuter/arpackpp.git
+		execute_process(COMMAND git clone https://github.com/m-reuter/arpackpp.git
 						WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}")
 		execute_process(COMMAND mkdir -p arpackpp_lib
 						WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}")
